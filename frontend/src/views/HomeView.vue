5566ec35a020f1acfe6071a23e5b58f09205c90b
srestrep74 <srestrep74@eafit.edu.co>
2024-08-10 14:19:46 -0500

    Initiate vue project

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <div class="card">
+      <div class="card-header">
+        Movies
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Movies Application</h5>
+        <p class="card-text">Welcome to the Movies Application</p>
+        <img src="https://images.pexels.com/photos/3137890/pexels-photo-3137890.jpeg" class="img-fluid" alt="img" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+};
+</script>
